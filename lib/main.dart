cdba93374a51514541aaa23942055cc137846d6b
anusree <anusreee61@gmail.com>
2023-06-22 21:01:21 -0700

    Complete task 3

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:quiz_app2/quiz.dart';
+
+
+void main() {
+  runApp(const Quiz());
+  
+}
+
+            
---
+++ lib/quiz.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app2/data/questions.dart';
+import 'package:quiz_app2/start_screen.dart';
+import 'package:quiz_app2/questions_screen.dart';
+import 'package:quiz_app2/results_screen.dart';
+
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> _selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    _selectedAnswers.add(answer);
+
+    if (_selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+  
+  
+  void restartQuiz() {
+    setState(() {
+       _selectedAnswers = [];
+       activeScreen = 'start-screen';
+
+    });
+  }
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 79, 4, 29),
+                Color.fromARGB(255, 163, 23, 178),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
